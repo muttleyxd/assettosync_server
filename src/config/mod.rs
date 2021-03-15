dfbc1044830aa4c5f60413f48c613bb374766ef1
Mateusz Szychowski (Muttley) <mateusz@szychowski.it>
2021-03-15 10:45:42 +0100

    initial commit

---
+++ src/config/mod.rs
@@ -0,0 +1,220 @@
+use std::fs::File;
+use std::io;
+use std::path::Path;
+use std::{borrow::Borrow, hash, io::Read};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct AssettoMod {
+    pub checksum_md5: String,
+    pub filename: String,
+    pub size_in_bytes: u64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct User {
+    pub login: String,
+    pub password_hash_sha512: String,
+    pub is_admin: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    pub mods: Vec<AssettoMod>,
+    pub mod_storage_location: String,
+    pub users: Vec<User>,
+}
+
+pub trait ConfigTrait {
+    fn new(path: &str) -> Self;
+    fn add_user(
+        &mut self,
+        login: &String,
+        new_password: &String,
+        is_admin: bool,
+    ) -> Result<(), &str>;
+    fn change_user_password(&mut self, login: &String, new_password: &String) -> Result<(), &str>;
+    fn delete_mod(&mut self, checksum_md5: &String) -> Result<(), String>;
+    fn delete_user(&mut self, login: &String) -> Result<(), &str>;
+    fn get_mod_list(&self) -> Vec<AssettoMod>;
+    fn is_login_data_valid(&self, login: &String, password: &String) -> bool;
+    fn is_user_admin(&self, login: &String) -> bool;
+    fn rebuild_mod_storage(&mut self, clear: bool) -> Result<(), String>;
+    fn user_exists(&self, login: &String) -> bool;
+}
+
+pub struct ConfigObject {
+    pub config: Config,
+    pub path: String,
+}
+
+fn get_assetto_mod(path: &Path) -> Result<AssettoMod, String> {
+    let input = File::open(path);
+    if let Err(error) = input {
+        return Err(error.to_string());
+    }
+    let mut file = input.unwrap();
+
+    use md5::{Digest, Md5};
+    let mut hasher = Md5::new();
+    let bytes_processed = io::copy(&mut file, &mut hasher);
+    if let Err(error) = bytes_processed {
+        return Err(error.to_string());
+    }
+    let hash = hasher.finalize();
+
+    Ok(AssettoMod {
+        checksum_md5: format!("{:x}", hash),
+        filename: path.file_name().unwrap().to_str().unwrap().to_string(),
+        size_in_bytes: bytes_processed.unwrap(),
+    })
+}
+
+fn write_config_to_json(path: &Path, config: &Config) {
+    let json = serde_json::to_string_pretty(config);
+    if let Ok(output) = json {
+        std::fs::write(path, output).expect("Config file writing failure");
+    }
+}
+
+fn calculate_sha512(value: &String) -> String {
+    use sha2::{Digest, Sha512};
+    let mut hasher = Sha512::new();
+    hasher.update(value);
+    let hash = hasher.finalize();
+    return format!("{:x}", hash);
+}
+
+impl ConfigTrait for ConfigObject {
+    fn new(path: &str) -> ConfigObject {
+        let file = std::fs::read_to_string(path);
+        let content = file.unwrap();
+
+        ConfigObject {
+            config: serde_json::from_str(content.as_ref()).unwrap(),
+            path: path.to_string(),
+        }
+    }
+
+    fn add_user(
+        &mut self,
+        login: &String,
+        new_password: &String,
+        is_admin: bool,
+    ) -> Result<(), &str> {
+        if self.user_exists(login) {
+            return Err("User already exists");
+        }
+
+        self.config.users.push(User {
+            login: login.clone(),
+            password_hash_sha512: calculate_sha512(new_password),
+            is_admin: is_admin,
+        });
+
+        write_config_to_json(Path::new(&self.path), &self.config);
+
+        Ok(())
+    }
+
+    fn change_user_password(&mut self, login: &String, new_password: &String) -> Result<(), &str> {
+        for user in self.config.users.iter_mut() {
+            if *login == user.login {
+                use sha2::{Digest, Sha512};
+                let mut hasher = Sha512::new();
+                hasher.update(new_password);
+                let hash = hasher.finalize();
+
+                user.password_hash_sha512 = format!("{:x}", hash);
+                write_config_to_json(Path::new(&self.path), &self.config);
+
+                return Ok(());
+            }
+        }
+        Err("User not found")
+    }
+
+    fn delete_mod(&mut self, checksum_md5: &String) -> Result<(), String> {
+        let index = self.config.mods.iter().position(|acmod| acmod.checksum_md5 == *checksum_md5);
+        if let None = index {
+            return Err("Mod not found".to_string());
+        }
+        let index = index.unwrap();
+
+        let storage_path = Path::new(&self.config.mod_storage_location);
+        let acmod = &self.config.mods[index];
+        
+        if let Err(error) = std::fs::remove_file(storage_path.join(&acmod.filename)) {
+            return Err(error.to_string());
+        }
+        self.config.mods.remove(index);
+        Ok(())
+    }
+
+    fn delete_user(&mut self, login: &String) -> Result<(), &str> {
+        self.config.users.retain(|user| user.login != *login);
+        Ok(())
+    }
+
+    fn get_mod_list(&self) -> Vec<AssettoMod> {
+        return self.config.mods.clone();
+    }
+
+    fn is_login_data_valid(&self, login: &String, password: &String) -> bool {
+        for user in self.config.users.iter() {
+            if *login == user.login {
+                return calculate_sha512(password) == user.password_hash_sha512;
+            }
+        }
+        false
+    }
+
+    fn is_user_admin(&self, login: &String) -> bool {
+        for user in self.config.users.iter() {
+            if *login == user.login {
+                return user.is_admin;
+            }
+        }
+        false
+    }
+
+    fn rebuild_mod_storage(&mut self, clear: bool) -> Result<(), String> {
+        let paths = std::fs::read_dir(&self.config.mod_storage_location);
+        if let Err(error) = paths {
+            return Err(error.to_string());
+        }
+
+        if clear {
+            self.config.mods.clear();
+        }
+        let mut mod_list: Vec<AssettoMod> = self.config.mods.clone();
+
+        for path in paths.unwrap() {
+            if let Err(error) = path {
+                return Err(error.to_string());
+            }
+            let mod_path = path.unwrap().path();
+            let exists = mod_list
+                .iter()
+                .any(|acmod| acmod.filename == mod_path.file_name().unwrap().to_str().unwrap());
+
+            if exists {
+                continue;
+            }
+
+            let assetto_mod = get_assetto_mod(mod_path.as_path())?;
+            mod_list.push(assetto_mod);
+        }
+
+        self.config.mods = mod_list;
+
+        write_config_to_json(Path::new(&self.path), &self.config);
+
+        Ok(())
+    }
+
+    fn user_exists(&self, login: &String) -> bool {
+        self.config.users.iter().any(|user| user.login == *login)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,581 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+use clap::{load_yaml, App};
+use md5::digest::Output;
+use rocket::Data;
+use rocket::{http::ContentType, response::NamedFile};
+use rocket_multipart_form_data::{
+    mime::{self, Name},
+    MultipartFormData, MultipartFormDataField, MultipartFormDataOptions, Repetition,
+};
+use std::{fs::File, path::{Path, PathBuf}};
+use std::{borrow::Borrow, collections::HashMap, sync::RwLock};
+
+mod config;
+
+use config::{ConfigObject, ConfigTrait};
+use rocket::http::{Cookie, Cookies};
+use rocket::request::Form;
+use rocket::response::{Flash, Redirect};
+use rocket::State;
+use rocket::{
+    request::{self, FlashMessage, FromRequest, Request},
+    response::content,
+};
+use rocket_contrib::{json::Json, templates::Template};
+
+fn get_user_name_from_cookie(cookies: &mut Cookies) -> Option<String> {
+    let cookie = cookies.get_private("user_name");
+    match cookie {
+        Some(cookie) => Some(cookie.value().to_string()),
+        None => None,
+    }
+}
+
+#[get("/")]
+fn index(
+    flash: Option<FlashMessage<'_, '_>>,
+    mut cookies: Cookies,
+    config_lock: State<RwLock<ConfigObject>>,
+) -> Result<Template, Flash<Redirect>> {
+    let user_name = get_user_name_from_cookie(&mut cookies);
+    if user_name.is_none() {
+        return Err(Flash::error(
+            Redirect::to(uri!(login_page_get)),
+            "You need to be logged in to view this page",
+        ));
+    }
+    std::mem::drop(cookies);
+
+    let config = config_lock.read().unwrap();
+    let user_name = user_name.unwrap();
+    let is_admin = config.is_user_admin(&user_name);
+
+    let mut context = HashMap::new();
+    context.insert("user_name", user_name);
+    if is_admin {
+        context.insert("admin", is_admin.to_string());
+    }
+    if let Some(ref msg) = flash {
+        context.insert("flash", msg.msg().to_string());
+        if msg.name() == "error" {
+            context.insert("flash_type", "Error".to_string());
+        }
+    }
+    Ok(Template::render("index", &context))
+}
+
+#[get("/login")]
+fn login_page_get(flash: Option<FlashMessage<'_, '_>>) -> Template {
+    let mut context = HashMap::new();
+    if let Some(ref msg) = flash {
+        context.insert("flash", msg.msg());
+        if msg.name() == "error" {
+            context.insert("flash_type", "Error");
+        }
+    }
+
+    Template::render("login", &context)
+}
+
+#[derive(FromForm)]
+struct LoginData {
+    login: String,
+    password: String,
+}
+
+#[post("/login", data = "<data>")]
+fn login_page_post(
+    data: Form<LoginData>,
+    mut cookies: Cookies,
+    config_lock: State<RwLock<ConfigObject>>,
+) -> Result<Redirect, Flash<Redirect>> {
+    let config = config_lock.read().unwrap();
+
+    let authentication_success = config.is_login_data_valid(&data.login, &data.password);
+    if !authentication_success {
+        return Err(Flash::error(
+            Redirect::to(uri!(login_page_get)),
+            "Wrong username/password",
+        ));
+    }
+    cookies.add_private(Cookie::new("user_name", data.login.clone()));
+    Ok(Redirect::to(uri!(index)))
+}
+
+#[post("/logout")]
+fn logout(mut cookies: Cookies) -> Redirect {
+    cookies.remove_private(Cookie::named("user_name"));
+    Redirect::to(uri!(login_page_get))
+}
+
+#[get("/style.css")]
+fn style_css() -> content::Css<&'static str> {
+    content::Css(include_str!("resources/style.css"))
+}
+
+#[derive(serde::Serialize)]
+struct UserTemplate {
+    login: String,
+    is_admin: bool,
+}
+
+#[derive(serde::Serialize)]
+struct UserManagementTemplateContext {
+    flash: Option<String>,
+    flash_type: Option<String>,
+    user_name: String,
+    users: Vec<UserTemplate>,
+}
+
+#[get("/change_password?<username>")]
+fn change_password_get(
+    flash: Option<FlashMessage<'_, '_>>,
+    mut cookies: Cookies,
+    username: String,
+    config_lock: State<RwLock<ConfigObject>>,
+) -> Result<Template, Flash<Redirect>> {
+    let user_name = get_user_name_from_cookie(&mut cookies);
+    if user_name.is_none() {
+        return Err(Flash::error(
+            Redirect::to(uri!(login_page_get)),
+            "You need to be logged in to view this page",
+        ));
+    }
+    std::mem::drop(cookies);
+
+    let config = config_lock.read().unwrap();
+    let user_name = user_name.unwrap();
+    let is_admin = config.is_user_admin(&user_name);
+
+    if !is_admin && username != user_name {
+        return Err(Flash::error(Redirect::to(uri!(index)), "Access denied"));
+    }
+
+    let mut context = HashMap::new();
+    context.insert("user_name", username);
+    if is_admin {
+        context.insert("is_admin", "yes".to_string());
+    }
+    if let Some(ref msg) = flash {
+        context.insert("flash", msg.msg().to_string());
+        if msg.name() == "error" {
+            context.insert("flash_type", "Error".to_string());
+        }
+    }
+    Ok(Template::render("change_password", &context))
+}
+
+#[derive(FromForm)]
+struct ChangePasswordData {
+    login: String,
+    password1: String,
+    password2: String,
+}
+
+#[post("/change_password", data = "<data>")]
+fn change_password_post(
+    data: Form<ChangePasswordData>,
+    mut cookies: Cookies,
+    config_lock: State<RwLock<ConfigObject>>,
+) -> Result<Flash<Redirect>, Flash<Redirect>> {
+    let user_name = get_user_name_from_cookie(&mut cookies);
+    if user_name.is_none() {
+        return Err(Flash::error(Redirect::to(uri!(index)), "Access denied"));
+    }
+    std::mem::drop(cookies);
+
+    let mut config = config_lock.write().unwrap();
+    let user_name = user_name.unwrap();
+    let is_admin = config.is_user_admin(&user_name);
+
+    if !is_admin && data.login != user_name {
+        return Err(Flash::error(Redirect::to(uri!(index)), "Access denied"));
+    }
+
+    if data.password1 != data.password2 {
+        return Err(Flash::error(
+            Redirect::to(uri!(change_password_get: data.login.clone())),
+            "Passwords are not the same",
+        ));
+    }
+
+    let result = config.change_user_password(&data.login, &data.password1);
+    if let Err(error) = result {
+        return Err(Flash::error(
+            Redirect::to(uri!(change_password_get: data.login.clone())),
+            error,
+        ));
+    }
+
+    if !is_admin {
+        return Ok(Flash::success(
+            Redirect::to(uri!(index)),
+            "Password changed",
+        ));
+    }
+    Ok(Flash::success(
+        Redirect::to(uri!(user_management)),
+        "Password changed",
+    ))
+}
+
+#[derive(serde::Serialize)]
+struct ModTemplate {
+    checksum_md5: String,
+    filename: String,
+    size_in_megabytes: u64,
+}
+
+#[derive(serde::Serialize)]
+struct ModManagementTemplateContext {
+    flash: Option<String>,
+    flash_type: Option<String>,
+    mods: Vec<ModTemplate>,
+    user_name: String,
+}
+
+use rocket::response::{self, Response, Responder};
+
+struct AssettoModResponse {
+    acmod: config::AssettoMod,
+    full_path: PathBuf,
+}
+
+impl<'a> Responder<'a> for AssettoModResponse {
+    fn respond_to(self, _: &Request) -> response::Result<'a> {
+        Response::build()
+            .header(ContentType::ZIP)
+            .raw_header("Content-Disposition", format!("inline; filename=\"{}\"", self.acmod.filename))
+            .chunked_body(File::open(&self.full_path).unwrap(), 8192)
+            .ok()
+    }
+}
+
+#[get("/mod_management/download?<hash>")]
+fn mod_download(
+    hash: String,
+    mut cookies: Cookies,
+    config_lock: State<RwLock<ConfigObject>>,
+) -> Result<AssettoModResponse, Flash<Redirect>> {
+    let user_name = get_user_name_from_cookie(&mut cookies);
+    if user_name.is_none() {
+        return Err(Flash::error(
+            Redirect::to(uri!(mod_management)),
+            "You need to be logged in to view this page",
+        ));
+    }
+    std::mem::drop(cookies);
+
+    let config = config_lock.read().unwrap();
+
+    for acmod in config.config.mods.iter() {
+        if acmod.checksum_md5 == hash {
+            let mod_storage = Path::new(&config.config.mod_storage_location);
+            return Ok(
+                AssettoModResponse
+                {
+                    acmod: acmod.clone(),
+                    full_path: mod_storage.join(&acmod.filename),
+                });
+        }
+    }
+
+    return Err(Flash::error(
+        Redirect::to(uri!(mod_management)),
+        "Mod hash not found",
+    ));
+}
+
+#[get("/mod_management/delete?<hash>")]
+fn mod_delete(
+    hash: String,
+    mut cookies: Cookies,
+    config_lock: State<RwLock<ConfigObject>>,
+) -> Result<Flash<Redirect>, Flash<Redirect>> {
+    let user_name = get_user_name_from_cookie(&mut cookies);
+    if user_name.is_none() {
+        return Err(Flash::error(
+            Redirect::to(uri!(login_page_get)),
+            "You need to be logged in to view this page",
+        ));
+    }
+    std::mem::drop(cookies);
+
+    let mut config = config_lock.write().unwrap();
+    let user_name = user_name.unwrap();
+    let is_admin = config.is_user_admin(&user_name);
+
+    if !is_admin {
+        return Err(Flash::error(Redirect::to(uri!(index)), "Access denied"));
+    }
+
+    if let Err(error) = config.delete_mod(&hash) {
+        return Err(Flash::error(Redirect::to(uri!(index)), error));
+    }
+
+    Ok(Flash::success(
+        Redirect::to(uri!(mod_management)),
+        "Mod deleted successfully.",
+    ))
+}
+
+#[get("/mod_management")]
+fn mod_management(
+    flash: Option<FlashMessage<'_, '_>>,
+    mut cookies: Cookies,
+    config_lock: State<RwLock<ConfigObject>>,
+) -> Result<Template, Flash<Redirect>> {
+    let user_name = get_user_name_from_cookie(&mut cookies);
+    if user_name.is_none() {
+        return Err(Flash::error(
+            Redirect::to(uri!(login_page_get)),
+            "You need to be logged in to view this page",
+        ));
+    }
+    std::mem::drop(cookies);
+
+    let config = config_lock.read().unwrap();
+    let user_name = user_name.unwrap();
+    let is_admin = config.is_user_admin(&user_name);
+
+    if !is_admin {
+        return Err(Flash::error(Redirect::to(uri!(index)), "Access denied"));
+    }
+
+    let mut context = ModManagementTemplateContext {
+        flash: None,
+        flash_type: None,
+        mods: vec![],
+        user_name: user_name,
+    };
+
+    for acmod in config.config.mods.iter() {
+        context.mods.push(ModTemplate {
+            checksum_md5: acmod.checksum_md5.clone(),
+            filename: acmod.filename.clone(),
+            size_in_megabytes: acmod.size_in_bytes / 1024 / 1024,
+        });
+    }
+
+    if let Some(ref msg) = flash {
+        context.flash = Some(msg.msg().to_string());
+        if msg.name() == "error" {
+            context.flash_type = Some("Error".to_string());
+        }
+    }
+    Ok(Template::render("mod_management", &context))
+}
+
+#[derive(serde::Serialize)]
+struct JsonModTemplate {
+    checksum_md5: String,
+    filename: String,
+    size_in_bytes: u64,
+}
+
+#[get("/mods.json")]
+fn mods_json(
+    mut cookies: Cookies,
+    config_lock: State<RwLock<ConfigObject>>,
+) -> Result<Json<Vec<JsonModTemplate>>, &'static str> {
+    let user_name = get_user_name_from_cookie(&mut cookies);
+    if user_name.is_none() {
+        return Err("Not logged in");
+    }
+    std::mem::drop(cookies);
+
+    let config = config_lock.read().unwrap();
+
+    let mut mods = vec![];
+    for acmod in config.config.mods.iter() {
+        mods.push(JsonModTemplate {
+            checksum_md5: acmod.checksum_md5.clone(),
+            filename: acmod.filename.clone(),
+            size_in_bytes: acmod.size_in_bytes,
+        });
+    }
+    Ok(Json(mods))
+}
+
+#[post("/mod_management/upload", data = "<data>")]
+fn mod_upload(
+    content_type: &ContentType,
+    data: Data,
+    mut cookies: Cookies,
+    config_lock: State<RwLock<ConfigObject>>,
+) -> Result<Flash<Redirect>, Flash<Redirect>> {
+    let user_name = get_user_name_from_cookie(&mut cookies);
+    if user_name.is_none() {
+        return Err(Flash::error(Redirect::to(uri!(index)), "Access denied"));
+    }
+    std::mem::drop(cookies);
+
+    let mut config = config_lock.write().unwrap();
+    let mod_path = Path::new(&config.config.mod_storage_location);
+
+    let options = MultipartFormDataOptions::with_multipart_form_data_fields(vec![
+        MultipartFormDataField::file("file[]")
+            .size_limit(4 * 1024 * 1024 * 1024)
+            .repetition(Repetition::fixed(50)),
+    ]);
+
+    let multipart_form_data = MultipartFormData::parse(content_type, data, options).unwrap();
+
+    let mut good_mods_count = 0;
+    let mut bad_mods_count = 0;
+    let archives = multipart_form_data.files.get("file[]");
+    if let Some(file_fields) = archives {
+        for file in file_fields {
+            if file.file_name.is_none() {
+                bad_mods_count += 1;
+                continue;
+            }
+
+            let file_name = file.file_name.clone().unwrap();
+            let path = std::path::Path::new(&file_name);
+            let extension = path.extension();
+            if extension.is_none() {
+                bad_mods_count += 1;
+                continue;
+            }
+
+            let extension = extension.unwrap().to_os_string();
+            if extension != "7z" && extension != "rar" && extension != "zip" {
+                bad_mods_count += 1;
+                continue;
+            }
+
+            let output_path = mod_path.join(file_name);
+            if output_path.exists() {
+                bad_mods_count += 1;
+                continue;
+            }
+
+            let result = std::fs::copy(&file.path, &output_path);
+            if let Err(error) = result {
+                println!(
+                    "Error while copying file from '{:?}' to '{:?}', reason: {}",
+                    &file.path, output_path, error
+                );
+                bad_mods_count += 1;
+                continue;
+            }
+
+            good_mods_count += 1;
+        }
+    }
+
+    if let Err(error) = config.rebuild_mod_storage(false) {
+        return Err(Flash::error(
+            Redirect::to(uri!(mod_management)),
+            format!(
+                "{} mods uploaded successfully, {} failed. Error while rebuilding mod storage: {}.",
+                good_mods_count, bad_mods_count, error
+            ),
+        ));
+    }
+
+    if bad_mods_count > 0 {
+        return Err(Flash::error(
+            Redirect::to(uri!(mod_management)),
+            format!(
+                "{} mods uploaded successfully, {} failed",
+                good_mods_count, bad_mods_count
+            ),
+        ));
+    }
+
+    Ok(Flash::success(
+        Redirect::to(uri!(mod_management)),
+        format!("{} mods uploaded successfully.", good_mods_count),
+    ))
+}
+
+#[get("/user_management")]
+fn user_management(
+    flash: Option<FlashMessage<'_, '_>>,
+    mut cookies: Cookies,
+    config_lock: State<RwLock<ConfigObject>>,
+) -> Result<Template, Flash<Redirect>> {
+    let user_name = get_user_name_from_cookie(&mut cookies);
+    if user_name.is_none() {
+        return Err(Flash::error(
+            Redirect::to(uri!(login_page_get)),
+            "You need to be logged in to view this page",
+        ));
+    }
+    std::mem::drop(cookies);
+
+    let config = config_lock.read().unwrap();
+    let user_name = user_name.unwrap();
+    let is_admin = config.is_user_admin(&user_name);
+
+    if !is_admin {
+        return Err(Flash::error(Redirect::to(uri!(index)), "Access denied"));
+    }
+
+    let mut context = UserManagementTemplateContext {
+        flash: None,
+        flash_type: None,
+        user_name: user_name,
+        users: vec![],
+    };
+
+    for user in config.config.users.iter() {
+        context.users.push(UserTemplate {
+            login: user.login.clone(),
+            is_admin: user.is_admin,
+        });
+    }
+
+    if let Some(ref msg) = flash {
+        context.flash = Some(msg.msg().to_string());
+        if msg.name() == "error" {
+            context.flash_type = Some("Error".to_string());
+        }
+    }
+    Ok(Template::render("user_management", &context))
+}
+
+fn main() {
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from(yaml).get_matches();
+    let mut config_file = "config.json";
+    if let Some(c) = matches.value_of("config") {
+        config_file = c;
+    }
+
+    let mut config = config::ConfigObject::new(config_file);
+    if config.get_mod_list().len() == 0 {
+        if let Err(error) = config.rebuild_mod_storage(false) {
+            panic!("Error loading {}: {}", config_file, error);
+        }
+    }
+
+    let pages = routes![
+        change_password_get,
+        change_password_post,
+        index,
+        login_page_get,
+        login_page_post,
+        logout,
+        mod_delete,
+        mod_download,
+        mod_management,
+        mod_upload,
+        mods_json,
+        style_css,
+        user_management
+    ];
+
+    let lock = RwLock::new(config);
+    rocket::ignite()
+        .manage(lock)
+        .mount("/", pages)
+        .attach(Template::fairing())
+        .launch();
+}
